8cb67140680bfe4ca699527759737da1f136dcd2
erroshankr <roshankumarjha24@gmail.com>
2024-03-28 22:03:56 +0530

    SL notes

--- src/inheritance/Animal.java
+++ src/inheritance/Animal.java
@@ -2,12 +2,16 @@ package inheritance;
 
 public class Animal {
 
-     String name;
+     String name; // Simbi
      String color;
 
     Animal(){
         System.out.println("Inside zero argument constructor of Animal");
+    }
 
+    Animal(String name){
+        System.out.println("Inside single argument constructor of Animal");
+        this.name= name;
     }
     Animal(String name, String color){
         System.out.println("Inside two argument constructor of Animal");
@@ -16,7 +20,11 @@ public class Animal {
     }
 
      void printDetails(String name, String color){
-        System.out.println("Name: " + name + " & Color: " + color);
+        System.out.println("Name: " + name + " & Color: " + color + " from Animal class");
+    }
+
+    void printDetails(String name, String color, boolean isWild, int noOfLegs){
+        System.out.println("name: " + name + " & color: " + color + " & wild: " + isWild + " & no of legs: " +noOfLegs );
     }
 
    /* public static void main(String[] args) {
--- src/inheritance/Lion.java
+++ src/inheritance/Lion.java
@@ -1,46 +1,85 @@
 package inheritance;
 
+
+import javax.xml.namespace.QName;
+import java.util.stream.IntStream;
+
+// super: immediate parent
+// Overriding:: Inheritance -> parent-child relation ship
+// Overloading :: Polymorphism -> may or may not have parent child relationship
 public class Lion extends Animal{ // default(Zero arg) constructor is mandatory in parent class for inheritance to work.
 
+    String name;  // Simba
     boolean isWild;
 
     Lion(){
+     //    Lion l2 = new Lion("Lion-8");
+        this("Lion-8"); // call to 2nd constructor from one must be the 1st call within 1st constructor
         System.out.println("Inside zero argument constructor of Lion");
+     //   super.printDetails("Tiger","Red");
     }
 
-    Lion(boolean isWild){
+    Lion(String name){
+        super("Dog");
         System.out.println("Inside single argument constructor of Lion");
-        this.isWild = isWild;
+        this.name = name;
+   //     super.name="Simbi";
+        System.out.println("name of parent is :"+ super.name );
+        super.printDetails(name,"");
     }
 
-    // name & return type : same & one in remaining 3 signature fiel must be false
+    // name & return type : same & one in remaining 3 signature field must be false
     void printDetails(String name, String color, boolean isWild){  // overloading
         System.out.println("Name: " + name + " & Color: " + color + " & isWild: " + isWild);
     }
 
-    //  signature: name, return type, access-specifier, number of parameters, sequence of parameters
-    void printDetails(String name, String color){  // method overriding -> signature of methods in parent & child class is exactly same
+    //  signature: name, return type, access-specifier, number of parameters, sequence of data type of parameters : must be same
+    void printDetails(String name, boolean isWild, String color){  // method overriding -> signature of methods in parent & child class is exactly same
         System.out.println("Name: " + name + " & Color: " + color + " & isWild: " + isWild);
     }
-    public static void main(String[] args) {
-        Lion l2 = new Lion();
-        Lion l1 = new Lion(true);
-        // On calling zero/parametrized constructor of child class, first call goes to zero arg constructor of immediate parent class.
-        l1.printDetails("Lion-1","black"); // compile time polymorphism, static binding
-        l1.printDetails("lion-3","red",true);
-        Animal a2 = new Animal();
-      //  a2.printDetails();// parent class object can not access child class behaviour
 
-        System.out.println(l1.getClass());
-        System.out.println("L1: "+l1.hashCode() + " & L2: " + l2.hashCode() );
+
+  /*  @Override
+    void printDetails(String name, String color){
+        System.out.println("Name: " + name + " & Color: " + color  + " from Lion class");
+    }*/
+    public static void main(String[] args) {
+    //    Lion l2 = new Lion();
+     //   Lion l1 = new Lion(true);
+        // On calling zero/parametrized constructor of child class, first call goes to zero arg constructor of immediate parent class provided there is no this/super call as 1st line within constructor.
+        // This rule doesn't apply for variables & methods.super call to variables & methods can be made at any line.
+     //   l1.printDetails("Lion-1","black"); // compile time polymorphism, static binding
+    //    l1.printDetails("lion-3","red",true);
+    //    Animal a2 = new Animal();
+    //    a2.printDetails("Lion-7","White");// parent class object can not access child class behaviour
+
+    //    System.out.println(l1.getClass());
+    //    System.out.println("L1: "+l1.hashCode() + " & L2: " + l2.hashCode() );
 
         //parent reference is storing child object
-        Animal a1 = new Lion(); //
+    //    Animal a1 = new Lion(); //
+    //    Animal a3 = new Lion(true);
       //  Lion l5 = new Animal(); child reference cant contain parent object
-        a1.printDetails("lion-4","brown"); // runtime polymorphism, dynamic binding
+    //    a1.printDetails("lion-4","brown"); // runtime polymorphism, dynamic binding
+      //  a1.printDetails("lion-6", false,"green"); // compilation error
 
     //    new Lion().printDetails();
     //    new Animal().printDetails();
+
+      //  Lion l1 = new Lion(); -- this
+    //    Animal a2 = new Animal("Simbi"); -- super
+  //      Animal a3 = new Animal();
+ //       System.out.println(a3.name);
+       /* Lion l2 = new Lion("Simba"); // Animal a4 = new Animal();
+        Lion l3 = new Lion("Simbaaaas");// a5
+        System.out.println("name of Lion is : " + l2.name);*/
+
+        Lion l1 = new Lion();
+
+
     }
 
+// super : acts as a reference/object of the immediate parent class
+// this: acts as an reference/object of the current class
+// Java doesn't support multiple inheritance  , however it gives essence of this thro interface
 }
