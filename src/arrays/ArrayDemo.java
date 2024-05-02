7738f069ed5b8301b6512b44c846c8b4be38999d
erroshankr <roshankumarjha24@gmail.com>
2024-05-02 22:37:12 +0530

    Array : data structure

--- src/arrays/ArrayDemo.java
+++ src/arrays/ArrayDemo.java
@@ -10,7 +10,19 @@ public class ArrayDemo {
     }*/
 
     // int,float,boolean,long,double,short,char,byte
-    public static void main(String[] args) {
+    static {
+        System.out.println("this is static block");
+    }
+    public static void main(String[] args, int x) {
+        System.out.println("inside overloaded method");
+    }
+       public static void main(String[] xyz) { // entry
+        System.out.println("inside main");
+           System.out.println("length of string array :" + xyz.length);
+           for (int i = 0; i < xyz.length; i++) {
+               System.out.println(xyz[i]);
+           }
+
      //   Boolean[] ar1 = new Boolean[5]; // size declaration is mandatory in array : index: 0,1,2,3,4
      //   boolean[] ar2 = new boolean[5];
      //   int[] ar = new int[5]; // It stores integer values
@@ -37,8 +49,6 @@ public class ArrayDemo {
         ar[0]= 5;
         ar[1]=6;
         ar[4]=15; // 5,6,0,0,10
-        ar[-2]= 20;
-        ar[10] = 100;
         /*for (int i = 0; i < ar.length; i++) {
             System.out.print(ar[i] + " ");
         }*/
@@ -54,5 +64,15 @@ public class ArrayDemo {
         // update at given index: O(1)
         // search : O(n)
         // access : O(1)
+        // {1,3,5,7,9} : length = 5, index: [0,1,2,3,4] --> ar[length] :- ArrayIndexofOutBound (2,7), (2,7], [2,7), [2,7],{2,7} -> {0,1,2,3,4}-
+        // ar[length - 1] : last element of the array
+       /* System.out.println(ar.length); // length is a property of an array: max index = length - 1
+        for (int i = 0; i <= ar.length-1; i++) {
+            System.out.println(ar[i]);  // ar[0],ar[1],ar[2],ar[3],ar[4]
+        }
+        for (int i = 0; i < ar.length; i++) {
+
+        }*/
+         // arrayOfInterest[1]
     }
 }
