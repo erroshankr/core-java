3a5a7d804dd34ce9cb7bd4c150d8a8e1ebebbad1
erroshankr <roshankumarjha24@gmail.com>
2024-05-03 21:58:03 +0530

    Array Problems

---
+++ src/arrays/ArrayBasedProblems.java
@@ -0,0 +1,52 @@
+package arrays;
+
+public class ArrayBasedProblems {
+
+    /*
+     Problem 1
+     int[] = {3,2,5,7}
+     */
+    public boolean isAscendingSortedArray(int[] input){
+        // if sorted return 'true' else false
+    }
+
+    /* Problem 2
+     Complete method below and return the number of elements strictly greater than X in given array
+     */
+    public int findElementCount(int[] input, int X){
+
+    }
+
+    /* Problem 3
+      Complete the below code to return an array having maximum & 2nd maximum value respectively
+      */
+    public int[] findMaxElements(int[] input){
+
+    }
+
+    /* Problem 4
+    Complete below code to return the maximum frequency of an element in the given array.
+     */
+    public int countMaxFreq(int[] nums){
+
+    }
+
+    /* Problem 5
+    Complete the code below to return the swapped array formed by exchanging the elements at x & y respectively. If given indices are invalid
+    return empty array.
+     */
+    public int[] swapElements(int[] input,int x, int y){
+
+    }
+
+    /*
+     Problem 6
+     Complete the code below to insert an element into the given array at a given index and return modified array. Given that index value can
+     be maximum the length of the array.
+     */
+
+    public int[] insertIntoArray(int[] input, int index, int value){
+
+    }
+
+}
