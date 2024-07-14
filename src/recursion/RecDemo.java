1412bfdea24560d3d44f7b177ddc754dbae3f952
erroshankr <roshankumarjha24@gmail.com>
2024-07-14 22:46:44 +0530

    Recursion

--- src/recursion/RecDemo.java
+++ src/recursion/RecDemo.java
@@ -5,7 +5,10 @@ public class RecDemo {
         // 5 : 5+4+3+2+1 = 15
        // System.out.println(reversal("RAM"));
        // printNum(5);
-        System.out.println(findSum(5)); // 15
+      //  System.out.println(findSum(5)); // 15
+     //  geoProgression(2,2,40);
+       printFibonacciSeries(10);
+
     }
 
 
@@ -49,6 +52,36 @@ public class RecDemo {
        }
        return reversal( str.substring(1)) + str.charAt(0); // AMR -> MRA  --> RAM
     }
+
+    public static void geoProgression(int num, int factor, int limit){ //2,2,40
+        if(num  > limit){
+            return;
+        }
+        System.out.println(num); // GP in ascending order
+        geoProgression(num*factor, factor, limit);
+        System.out.println(num); // GP in descending order
+
+    }
+
+    public static int fibonacciSeq(int n){ // (n-1) + (n-2)
+        if(n == 0){
+            return 0;
+        }else if(n == 1){
+            return 1;
+        }else{
+            return fibonacciSeq(n-1) + fibonacciSeq(n-2);
+        }
+    }
+
+    public static void printFibonacciSeries(int n){
+        for (int i = 0; i <= n; i++) {
+            System.out.println(fibonacciSeq(i));
+        }
+    }
+
+    //  0, 1, 1, 2, 3, 5, 8, 13
+
+    // num =2, factor =2 --> 2,4,8,16,32,64
 }
 
 //0 1 1 2 3 .....
