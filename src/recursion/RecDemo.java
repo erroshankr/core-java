7807b27247cd420bc0b934de0751f482fb14d426
erroshankr <roshankumarjha24@gmail.com>
2024-07-14 21:58:28 +0530

    Recursion

--- src/recursion/RecDemo.java
+++ src/recursion/RecDemo.java
@@ -3,15 +3,30 @@ package recursion;
 public class RecDemo {
     public static void main(String[] args) {
         // 5 : 5+4+3+2+1 = 15
-        System.out.println(reversal("RAM"));
+       // System.out.println(reversal("RAM"));
+       // printNum(5);
+        System.out.println(findSum(5)); // 15
     }
 
 
-    public static int findSum(int i) { // i = 5, sum = 0
+    public static void printNum(int num){ // ip= 5, op = 0,1,2,3,4,5 using recursion
+        if(num == 0){ // base condition/ terminating condition
+            return;
+        }
+        printNum(num -1); // printNum(3), num = 2
+        System.out.println(num); // 15
+    }
+
+    public static int findSum(int i) { // i = 5, sum = 5+4+3+2+1 = 15
         if(i == 1){
             return 1;
         }
         return findSum(i - 1) + i;
+
+       /* int sum = 0;
+        for (int j = 1; j <= i; j++) {
+            sum = sum + j;
+        }*/
     }
 
     public static int findFactorial(int num){
@@ -22,7 +37,7 @@ public class RecDemo {
     }
 
     public static int power(int x , int y){ // x^y
-        if(y==0){
+        if(y == 0){
             return 1;
         }
         return x * power(x,y-1);
